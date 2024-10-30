f257b5a47f6710dab74c4ae0f6a96d5a87e141cc
Dev <iwwwi2015@gmail.com>
2024-10-30 14:39:12 -0400

    v1.0.6

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'integration-helper/integration-version',
+        'pretty_version' => '1.0.5',
+        'version' => '1.0.5.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'integration-helper/integration-version' => array(
+            'pretty_version' => '1.0.5',
+            'version' => '1.0.5.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
